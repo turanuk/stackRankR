5238986c936499ae9627f7a5b2c3de4e9bccfc7f
markberr <markwberryman@yahoo.com>
2012-03-20 08:58:22 -0700

    - Processing the MONGOLAB_URI env var which is set on production deployments by hosting companies like AppHarbor and Heroku - Fixing numerous bugs w/ async waterfall code - More logging

--- dal/dataAccessLayer.js
+++ dal/dataAccessLayer.js
@@ -6,9 +6,46 @@ var mongo = require('mongodb'),
 
 var async = require('async');
 
-var databaseName = "stackRankR";
 var dataTableName = "teams";	// single table where we're storing all of the data
 var singleDataIdentifier = "1";	// only storing one piece of data in the database; we'll identify it w/ this value
+
+var mongoLabUri = (process.env.MONGOLAB_URI || false);
+
+if (mongoLabUri) {
+	var runningInProduction = true;
+
+	// trim off the 'mongodb://' piece
+	mongoLabUri = mongoLabUri.substring(10);
+
+	// get the username for connecting to db
+	var indexOfUsernameColonDelimiter = mongoLabUri.indexOf(':');
+	var databaseUser = mongoLabUri.substring(0, indexOfUsernameColonDelimiter);
+	mongoLabUri = mongoLabUri.substring(databaseUser.length + 1);
+
+	// get the password for connecting to db
+	var indexOfPasswordDelimiter = mongoLabUri.indexOf('@');
+	var databaseUserPassword = mongoLabUri.substring(0, indexOfPasswordDelimiter);
+	mongoLabUri = mongoLabUri.substring(databaseUserPassword.length + 1);
+
+	// get the db server name
+	var indexOfDbServerDelimiter = mongoLabUri.indexOf(':');
+	var databaseServer = mongoLabUri.substring(0, indexOfDbServerDelimiter);
+	mongoLabUri = mongoLabUri.substring(databaseServer.length + 1);
+
+	// get the db server port
+	var indexOfDbServerPortDelimiter = mongoLabUri.indexOf('/');
+	var databaseServerPort = mongoLabUri.substring(0, indexOfDbServerPortDelimiter);
+	mongoLabUri = mongoLabUri.substring(databaseServerPort.length + 1);
+	databaseServerPort = parseInt(databaseServerPort);	// mongodb driver requires port to be an int
+
+	// get the database name
+	var databaseName = mongoLabUri;
+} else {
+	var runningInProduction = false;
+	var databaseServer = 'localhost';
+	var databaseServerPort = 27017;
+	var databaseName = 'stackRankR';
+}
 
 var testData =
 	{ 
@@ -48,22 +85,48 @@ var testData =
 	};
 
 var openDb = function(callback) {
-	var server = new Server('localhost', 27017, {auto_reconnect: true});
+	console.log('Preparing to open connection to database server...');
+
+	console.log('Server: ' + databaseServer);
+	console.log('Server port: ' + databaseServerPort);
+	console.log('Database name: ' + databaseName);
+
+	var server = new Server(databaseServer, databaseServerPort, {auto_reconnect: true});
+
 	var db = new Db(databaseName, server);
+
+	console.log('Opening connection to database server...');
 
 	db.open(function(err, db) {
 		if (!err) {
-			console.log('Connection to db succeeded!');
+			console.log('Connection to database server succeeded!');
 
-			// create the collection; if it already exists, no-op
-			// TODO: move this out of here so it's not invoked on every call!
-			db.createCollection(dataTableName, function(err, collection) {
-				if (err) {
-					callback(err, db);
-				}
-
-				callback(null, db);
-			});
+			if (runningInProduction) {
+				db.authenticate(databaseUser, databaseUserPassword, function(err, result) {
+					if (!err) {
+						// create the collection; if it already exists, no-op
+						// TODO: move this out of here so it's not invoked on every call!
+						db.createCollection(dataTableName, function(err, collection) {
+							if (!err) {
+								callback(null, db);							
+							} else {
+								callback(err, db);
+							}
+						});
+					} else {
+						console.log('Database authentication failed');
+						callback(err, db);
+					}
+		    	});
+			} else {
+				db.createCollection(dataTableName, function(err, collection) {
+					if (!err) {
+						callback(null, db);
+					} else {
+						callback(err, db);
+					}
+				});
+			}
 		} else {
 			console.log('Connection to db FAILED!');
 
@@ -80,24 +143,25 @@ exports.getData = function(response) {
 
 	    // get the board
 	    function(db, callback) {
+			console.log('Trying to get the data');
+
 	    	db.collection(dataTableName, function(err, collection) {
 				collection.findOne({ "TeamId": singleDataIdentifier }, function(err, item) {
-					if (err) {
+					if (!err) {
+						if (item == null) {
+							console.log('Did NOT find the data.');
+						} else {
+							console.log('Found the data.');
+
+							response.writeHead(200, { "Content-Type": "application/json", "Cache-Control": "no-cache" });
+							// convert from a JSON object to a string
+							response.write(JSON.stringify(item));
+						}
+
+		        		callback(null, db);
+					} else {
 						callback(err, db);
 					}
-
-					if (item == null) {
-						console.log('Did NOT find the data.');
-					} else {
-						console.log('Found the data.');
-
-						response.writeHead(200, { "Content-Type": "application/json", "Cache-Control": "no-cache" });
-						// convert from a JSON object to a string
-						response.write(JSON.stringify(item));
-					}
-
-					// and we're done
-	        		callback(null, db);
 				});
 			});
 	    }	   
@@ -155,28 +219,28 @@ var saveDataInternal = function(db, data, callback) {
 	// for now, we'll just out the board and re-add it; no partial updates
 	db.collection(dataTableName, function(err, collection) {
 		collection.findOne({ "TeamId": singleDataIdentifier }, function(err, item) {
-			if (err) {
+			if (!err) {
+				if (item == null) {
+					console.log("Seeing the data for the first time, saving it.");
+
+					collection.insert(data);
+				} else {
+					collection.remove({ "TeamId": singleDataIdentifier });
+					collection.insert(data);
+				}
+
+				// create index; if exists, no-op
+				// we have to do this *after* a document is first inserted
+				collection.ensureIndex({ "TeamId": singleDataIdentifier });
+
+				console.log('Data saved.');
+
+				callback(null, db);
+			} else {
 				console.log("Unable to retrieve data. Save failed.");
 
 				callback(err, db);
 			}
-
-			if (item == null) {
-				console.log("Seeing the data for the first time, saving it.");
-
-				collection.insert(data);
-			} else {
-				collection.remove({ "TeamId": singleDataIdentifier });
-				collection.insert(data);
-			}
-
-			// create index; if exists, no-op
-			// we have to do this *after* a document is first inserted
-			collection.ensureIndex({ "TeamId": singleDataIdentifier });
-
-			console.log('Data saved.');
-
-			callback(null, db);			
 		});
 	});
 }
